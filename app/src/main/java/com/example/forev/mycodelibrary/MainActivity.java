6b66fcdfa0cd7795db8d6cfe720a5f32cac5d9bc
caoyali <caoyali@gmtech.ai>
2019-03-25 17:57:58 +0800

    feature: 提交线程的界面入口

--- app/src/main/java/com/example/forev/mycodelibrary/MainActivity.java
+++ app/src/main/java/com/example/forev/mycodelibrary/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends BaseActivity{
 
     @OnClick({R.id.animation_btn, R.id.view, R.id.mSelectFileBtn, R.id.mReflex, R.id.mPermissionRequest,
     R.id.mHookTest, R.id.mProviderTest, R.id.mServiceTest, R.id.mAlgorithmTest,
-    R.id.mSkill})
+    R.id.mSkill, R.id.mThread})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.animation_btn:
@@ -58,6 +58,9 @@ public class MainActivity extends BaseActivity{
                 break;
             case R.id.mSkill:
                 openActivity(SkillActivity.class);
+                break;
+            case R.id.mThread:
+                openActivity(ThreadActivity.class);
                 break;
         }
     }
