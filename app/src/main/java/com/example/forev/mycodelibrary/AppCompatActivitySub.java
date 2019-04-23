fc927ba599b16bd5f29a88e76e9c1276996c3b7a
caoyali <caoyali@gmtech.ai>
2019-04-23 12:03:15 +0800

    optimize: 增加约束布局动画demo

--- app/src/main/java/com/example/forev/mycodelibrary/AppCompatActivitySub.java
+++ app/src/main/java/com/example/forev/mycodelibrary/AppCompatActivitySub.java
@@ -2,11 +2,22 @@ package com.example.forev.mycodelibrary;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+
+import butterknife.OnClick;
 
 /**
  * 本类主要是测试ConstraintLayout的使用方式。主要体现在XML页面。
  */
 public class AppCompatActivitySub extends BaseActivity {
+    private ConstraintSet mConset1;
+    private ConstraintSet mConset2;
+    private ConstraintLayout mRoot;
+    boolean isChanged = false;
 
     @Override
     protected int getLayoutId() {
@@ -15,6 +26,29 @@ public class AppCompatActivitySub extends BaseActivity {
 
     @Override
     protected void initView() {
+        mRoot = (ConstraintLayout) findViewById(R.id.rootView);
+
+        mConset1 = new ConstraintSet();
+        mConset2 = new ConstraintSet();
+
+        mConset1.clone(mRoot);
+        mConset2.clone(this, R.layout.activity_constraint_test_2);
+
+    }
+
+    @OnClick({R.id.mBtn})
+    public void onClick(View v){
+        if (isChanged){
+            TransitionManager.beginDelayedTransition(mRoot);
+            mConset1.applyTo(mRoot);
+            isChanged = false;
+        } else {
+            AutoTransition transition = new AutoTransition();
+            transition.setDuration(300);
+            TransitionManager.beginDelayedTransition(mRoot,transition);
+            mConset2.applyTo(mRoot);
+            isChanged = true;
+        }
 
     }
 
