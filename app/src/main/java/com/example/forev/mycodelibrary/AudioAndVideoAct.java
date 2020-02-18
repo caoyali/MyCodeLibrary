88a5a910a4d992a6600442cbbdf879da91f3570b
曹娅丽 <caoyali@koolearn.com>
2020-02-18 14:38:48 +0800

    添加新的代码

---
+++ app/src/main/java/com/example/forev/mycodelibrary/AudioAndVideoAct.java
@@ -0,0 +1,17 @@
+package com.example.forev.mycodelibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AudioAndVideoAct extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_audeo_and_video;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
--- app/src/main/java/com/example/forev/mycodelibrary/MainActivity.java
+++ app/src/main/java/com/example/forev/mycodelibrary/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends BaseActivity{
 
     @OnClick({R.id.animation_btn, R.id.view, R.id.mSelectFileBtn, R.id.mReflex, R.id.mPermissionRequest,
     R.id.mHookTest, R.id.mServiceTest, R.id.mProviderTest,R.id.mAlgorithmTest,
-    R.id.mSkill, R.id.mThread, R.id.mMemory, R.id.mServer, R.id.mKotlin})
+    R.id.mSkill, R.id.mThread, R.id.mMemory, R.id.mServer, R.id.mKotlin, R.id.mAiSound, R.id.mAudioAndVideo})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.animation_btn:
@@ -61,6 +61,12 @@ public class MainActivity extends BaseActivity{
                 break;
             case R.id.mKotlin:
 //                openActivity();
+                break;
+            case R.id.mAiSound:
+                openActivity(AiSoundActivity.class);
+                break;
+            case R.id.mAudioAndVideo:
+                openActivity(AudioAndVideoAct.class);
                 break;
         }
     }
